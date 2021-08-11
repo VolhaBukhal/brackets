fe42ecf5aec0a610e726b6141b0b78c2664b845c
ola <o.bukhal@gmail.com>
2021-08-11 12:39:33 +0300

    fix: add logiс for identical brackets

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ module.exports = function check(str, bracketsConfig) {
   const openedBrackets = bracketsConfig.map( el => el[0]);
   const revesedSubArray = bracketsConfig.map( el => [ el[1], el[0] ]);
   const bracketPair = Object.fromEntries(revesedSubArray);
-
+  debugger;
   let stack = [];
   for (let i = 0; i < str.length; i++) {
     const currentSymbol = str[i];
 
-    if ( openedBrackets.includes(currentSymbol)  ) {
+    if ( openedBrackets.includes(currentSymbol) && bracketPair[currentSymbol] !== currentSymbol  ) {
       stack.push(currentSymbol);
-    } else {
-      if (stack.length === 0) {
-        return false;
+    } else if (bracketPair[currentSymbol] === currentSymbol && !stack.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+      } else {
+        if (stack.length === 0) {
+          return false;
       } 
+
       let topOfStack = stack[stack.length - 1];
 
       if(bracketPair[currentSymbol] === topOfStack) {
@@ -23,11 +26,5 @@ module.exports = function check(str, bracketsConfig) {
       } 
     }
   }
-
-  if (stack.length === 0) {
-    return true;
-  } else {
-    return false;
-  }
-  // return stack.length === 0; 
+  return stack.length === 0; 
 }
